3d4d1c29d54fd8bba5943e8542821722e55bb988
paulmorio <pms69@cam.ac.uk>
2020-04-30 20:09:41 +0100

    [Docs] documentation for the plot embedding method

--- usualsuspects/tsne.py
+++ usualsuspects/tsne.py
@@ -8,43 +8,43 @@ from matplotlib.colors import ListedColormap
 
 
 class Quick2DTSNE(object):
+	"""A quick tool for creating 2D T-SNE plots with input X, 
+	and optional true classifications y for coloring.
+	
+	From Scikit Docs:
+	t-Distributed Stochastic Neighbour Embedding is a tool 
+	for visualizing high-dimensional data. It converts similarities
+	between data points to joint probabilities and tries to minimize
+	the KL divergence between the joint probabilities of the low 
+	dimensional embedding and the high-dimensional data. T-SNE has a cost
+	function that is not convex. i.e. with different initializations
+	we are bound to have different results.
+
+	This tool is a shortcut to producing the T-SNE plot one finds
+	so often in papers.
+
+	Parameters
+	----------
+	X : array-like
+		asdfasdf
+	y : array-like
+		asdfasdf
+	perplexity : float
+		The perplexity is a hyperparameter that is related to 
+		the number of nearest neighbours to consider. Typically
+		larger datasets need a higher perplexity to look good.
+	initialization : str (default="pca")
+		Possible values = ['pca', 'random'] describes the initialization.
+	n_jobs : int (default=-1)
+		The number of cores to use. Default is -1 which uses all 
+		available system cores
+
+	Returns
+	-------
+	Quick2DTSNE
+	"""
+
 	def __init__(self, X, y, perplexity, initialisation="pca", n_jobs=-1):
-		"""A quick tool for creating 2D T-SNE plots with input X, 
-		and optional true classifications y for coloring.
-		
-		From Scikit Docs:
-		t-Distributed Stochastic Neighbour Embedding is a tool 
-		for visualizing high-dimensional data. It converts similarities
-		between data points to joint probabilities and tries to minimize
-		the KL divergence between the joint probabilities of the low 
-		dimensional embedding and the high-dimensional data. T-SNE has a cost
-		function that is not convex. i.e. with different initializations
-		we are bound to have different results.
-
-		This tool is a shortcut to producing the T-SNE plot one finds
-		so often in papers.
-
-		Parameters
-		----------
-		X : array-like
-			asdfasdf
-		y : array-like
-			asdfasdf
-		perplexity : float
-			The perplexity is a hyperparameter that is related to 
-			the number of nearest neighbours to consider. Typically
-			larger datasets need a higher perplexity to look good.
-		initialization : str (default="pca")
-			Possible values = ['pca', 'random'] describes the initialization.
-		n_jobs : int (default=-1)
-			The number of cores to use. Default is -1 which uses all 
-			available system cores
-
-		Returns
-		-------
-		Quick2DTSNE
-		"""
-
 		super(Quick2DTSNE, self).__init__()
 		self.X = X
 		self.y = y
@@ -60,6 +60,21 @@ class Quick2DTSNE(object):
 		self.embeddings = self.tsne.fit_transform(self.X)
 
 	def plot_embedding(self, title=None, save_to="tsne_plot.png"):
+		"""Creates a matplotlib pyplot and saves it as a png image.
+
+		Parameters
+		----------
+		title : str
+			Title for the plot
+		save_to : str
+			path and filename where the image will be saved. 
+			Extension is contextual but expects .png or .jpg
+
+		Returns
+		-------
+		None 
+			Saves a PNG image of the plot to the path in `save_to`
+		"""
 		x_min, x_max = np.min(self.embeddings,0), np.max(self.embeddings,0)
 		embs_to_plot = (self.embeddings-x_min) / (x_max-x_min)
 
